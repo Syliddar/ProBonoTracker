056522fdebbe3203df114910721d8bc6ea744e5e
Jason <Jason.myers8@gmail.com>
2016-12-24 21:26:15 -0600

    Merge Create/Edit views

--- src/MemigrationProBonoTracker/Controllers/AttorneyController.cs
+++ src/MemigrationProBonoTracker/Controllers/AttorneyController.cs
@@ -18,14 +18,12 @@ namespace MemigrationProBonoTracker.Controllers
             _context = context;
         }
 
-        // GET: Attorneys
         public IActionResult Index(bool? assigning)
         {
             var model = _context.GetAttorneyListingViewModel(assigning);
             return View(model);
         }
 
-        // GET: Attorneys/Details/5
         public IActionResult Details(int? id)
         {
             if (id == null)
@@ -36,22 +34,22 @@ namespace MemigrationProBonoTracker.Controllers
             return View(attorney);
         }
 
-        // GET: Attorneys/Create
-        public IActionResult Create()
+        public IActionResult CreateEdit(int id = 0)
         {
-            return View();
+            var model = new Attorney();
+            if (id != 0)
+            {
+                model = _context.GetAttorneyDetails(id);
+            }
+            return View("CreateEdit", model);
         }
-
-        // POST: Attorneys/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Attorney attorney)
+        public IActionResult Save(Attorney attorney)
         {
             if (ModelState.IsValid)
             {
-                var success = _context.AddAttorney(attorney);
+                var success = attorney.Id == 0 ? _context.AddAttorney(attorney) : _context.UpdateAttorney(attorney);
                 if (success == 1)
                 {
                     return RedirectToAction("Index");
@@ -61,52 +59,9 @@ namespace MemigrationProBonoTracker.Controllers
             return View(attorney);
         }
 
-        // GET: Attorneys/Edit/5
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var attorney = _context.GetAttorneyDetails(id.Value);
-            return View(attorney);
-        }
-
-        // POST: Attorneys/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Attorney attorney)
-        {
-            if (id != attorney.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    var success = _context.UpdateAttorney(attorney);
-                    if (success == 1)
-                    {
-                        return RedirectToAction("Index");
-                    }
-                    ModelState.AddModelError(string.Empty, "An internal error occurred. Please try again later.");
-                }
-                catch (Exception)
-                {
-                    ModelState.AddModelError(string.Empty, "An internal error occurred. Please try again later.");
-                }
-                return RedirectToAction("Index");
-            }
-            return View(attorney);
-        }
-        // POST: Attorneys/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
+        public IActionResult Delete(int id)
         {
             var success = _context.DeleteAttorney(id);
             if (success == 1)
