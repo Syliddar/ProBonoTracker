1ea54232212bef434b620a3693293cc774524c28
Jason <Jason.myers8@gmail.com>
2016-12-28 18:11:29 -0600

    CaseEvent on create Case completed

--- src/MemigrationProBonoTracker/wwwroot/js/site.js
+++ src/MemigrationProBonoTracker/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿// Write your Javascript code.
+
+$().ready(function () {
+    initDatePickers();
+
+});
+
+
+function initDatePickers() {
+    $(".datepicker")
+            .datepicker({
+                format: "dd-M-yyyy",
+                startDate: "-1d",
+                todayBtn: "linked",
+                autoclose: true,
+                todayHighlight: true
+            });
+}
